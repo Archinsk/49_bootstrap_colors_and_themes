70375da7012d3bb5b5f1e24591e004b993ed5d91
Mikhail <mgp-nsk@yandex.ru>
2022-03-14 08:05:43 +0700

    start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container">
+    <div class="w-100 d-flex flex-column">
+      <div class="bg-royal">bg-royal</div>
+      <div class="bg-frosty">bg-frosty</div>
+      <div class="bg-turquoise">bg-turquoise</div>
+      <div class="bg-malachite">bg-malachite</div>
+      <div class="bg-lime">bg-lime</div>
+      <div class="bg-pear">bg-pear</div>
+      <div class="bg-sunrise">bg-sunrise</div>
+      <div class="bg-carrot">bg-carrot</div>
+      <div class="bg-cherry">bg-cherry</div>
+      <div class="bg-fuchsia">bg-fuchsia</div>
+      <div class="bg-orchid">bg-orchid</div>
+      <div class="bg-violet">bg-violet</div>
+    </div>
+    <div>
+      <button class="btn btn-royal">btn-royal</button>
+      <button class="btn btn-frosty">btn-frosty</button>
+      <button class="btn btn-turquoise">btn-turquoise</button>
+      <button class="btn btn-malachite">btn-malachite</button>
+      <button class="btn btn-lime">btn-lime</button>
+      <button class="btn btn-pear">btn-pear</button>
+      <button class="btn btn-sunrise">btn-sunrise</button>
+      <button class="btn btn-carrot">btn-carrot</button>
+      <button class="btn btn-cherry">btn-cherry</button>
+      <button class="btn btn-fuchsia">btn-fuchsia</button>
+      <button class="btn btn-orchid">btn-orchid</button>
+      <button class="btn btn-violet">btn-violet</button>
+    </div>
+    <div>
+      <button class="btn btn-outline-royal">btn-outline-royal</button>
+      <button class="btn btn-outline-frosty">btn-outline-frosty</button>
+      <button class="btn btn-outline-turquoise">btn-outline-turquoise</button>
+      <button class="btn btn-outline-malachite">btn-outline-malachite</button>
+      <button class="btn btn-outline-lime">btn-outline-lime</button>
+      <button class="btn btn-outline-pear">btn-outline-pear</button>
+      <button class="btn btn-outline-sunrise">btn-outline-sunrise</button>
+      <button class="btn btn-outline-carrot">btn-outline-carrot</button>
+      <button class="btn btn-outline-cherry">btn-outline-cherry</button>
+      <button class="btn btn-outline-fuchsia">btn-outline-fuchsia</button>
+      <button class="btn btn-outline-orchid">btn-outline-orchid</button>
+      <button class="btn btn-outline-violet">btn-outline-violet</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
